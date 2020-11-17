c551fd1052d51d100ccdad0c8cf92357f279382c
uzinok_HP <uzinok@yandex.ru>
2020-11-18 02:30:59 +0400

    начал писать скрипт 18.11.2020:2:30

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,27 @@
-// import {asd} from './func';
+window.onload = () => {
+  let popup = new Bolt(document.querySelector('.btn'));
+}
 
-// asd();
+class Bolt {
+  constructor(btn) {
+    this.btn = btn,
+    this.popup = document.getElementById(this.btn.getAttribute('data-bolt')),
+
+    this.initB()
+  }
+
+  initB() {
+    // tabindex -1 интерактивным элементам окна
+
+    // a[href]:not([tabindex='-1']),
+    // area[href]:not([tabindex='-1']),
+    // input:not([disabled]):not([tabindex='-1']),
+    // select:not([disabled]):not([tabindex='-1']),
+    // textarea:not([disabled]):not([tabindex='-1']),
+    // button:not([disabled]):not([tabindex='-1']),
+    // iframe:not([tabindex='-1']),
+    // [tabindex]:not([tabindex='-1']),
+    // [contentEditable=true]:not([tabindex='-1'])
+  }
+
+}
