a65ba0a6459ee60a595567558c271d513623e038
uzinok <uzinok@yandex.ru>
2020-11-23 18:58:00 +0400

    осталось поправить скрол, закрытие по подложке

--- src/js/main.js
+++ src/js/main.js
@@ -10,11 +10,11 @@ window.onload = () => {
 class Bolt {
   constructor(btn) {
     // кнопка
-    this.btn = btn,
-      // окно
-      this.popup = document.getElementById(this.btn.getAttribute('data-bolt')),
-      // css селектор интерактивных окон
-      this.interactiveCSS = `
+    this.btn = btn
+    // окно
+    this.popup = document.getElementById(this.btn.getAttribute('data-bolt'))
+    // css селектор интерактивных окон
+    this.interactiveCSS = `
         a[href]:not([tabindex='-1']),
         area[href]:not([tabindex='-1']),
         input:not([disabled]):not([tabindex='-1']),
@@ -26,9 +26,10 @@ class Bolt {
         [contentEditable=true]:not([tabindex='-1'])
       `,
       // инициализация окна
-      this.initB(),
-      // отслеживаем действия кнопки и открытого окна
-      this.influence()
+      this.initB()
+    // отслеживаем действия кнопки и открытого окна
+    this.influence()
+    this.check = false
   }
 
   initB() {
@@ -55,86 +56,83 @@ class Bolt {
 
   // отслеживаем действия кнопки и открытого окна
   influence() {
-    // клик по кнопке
     if (this.popup) {
 
-      this.btn.addEventListener('click', {
-        handleEvent: () => {
+      // открытие окна
+      this.btn.addEventListener("click", this.open.bind(null, this), false);
 
+      // закрытие по кнопке
+      this.popup.querySelector('.bolt-close').addEventListener("click", this.close.bind(null, this), false);
 
-          this.popup.querySelector('.bolt-close').addEventListener('click', {
-            handleEvent: this.close,
-            obj: this
-          });
-
-          // this.popup.addEventListener('click', {
-          //   handleEvent: () => {
-          //     if(this){
-          //       console.log(e);
-          //     }
-          //   },
-          //   obj: this
-          // });
-
-          // document.addEventListener('keydown', ()=>{
-          //   console.log(this);
-          // });
-
-          document.body.style.paddingRight = window.innerWidth - document.body.offsetWidth + 'px';
-          setTimeout(() => {
-            document.body.style.overflow = 'hidden';
-          }, 0)
-
-          if (document.querySelectorAll(`[data-bolt=${this.btn.getAttribute('data-bolt')}]`).length == 1) {
-            this.popup.style.top = '';
-            this.popup.style.left = '';
-          }
-
-          this.popup.removeAttribute('aria-hidden');
-          this.popup.classList.remove('bolt-hidden');
-
-          let interactiveEl = document.querySelectorAll(this.interactiveCSS);
-          // перебираем все элементы
-          for (let i = 0; i < interactiveEl.length; i++) {
-            // если уже есть nabindex
-            if (interactiveEl[i].getAttribute('tabindex')) {
-              // задаем атрибут что бы запомнить nabindex
-              interactiveEl[i].setAttribute('data-tabindex', interactiveEl[i].getAttribute('tabindex'))
-            }
-            // задаем nabindex -1
-            interactiveEl[i].setAttribute('tabindex', -1);
-          }
-
-          let interactivePopup = this.popup.querySelectorAll('[data-p]');
-
-          for (let i = 0; i < interactivePopup.length; i++) {
-            if (interactivePopup[i].getAttribute('data-tabindex')) {
-              interactivePopup[i].setAttribute('tabindex', interactivePopup[i].getAttribute('data-tabindex'))
-            } else {
-              interactivePopup[i].removeAttribute('tabindex');
-            }
-          }
-
-          interactivePopup[0].focus();
-        },
-        obj: this
-      });
-
+      // закрытие по esk
+      document.addEventListener("keydown", this.keydownEvent.bind(null, this), false);
     }
   }
 
-  close() {
-    this.obj.popup.setAttribute('aria-hidden', true);
-    this.obj.popup.classList.add('bolt-hidden');
+  open(obj) {
+    obj.check = true;
 
-    let interactivePopup = this.obj.popup.querySelectorAll('[data-p]');
+    // открываем окно читалкам
+    obj.popup.removeAttribute('aria-hidden');
+    // показываем окно
+    obj.popup.classList.remove('bolt-hidden');
+
+    if (document.querySelectorAll(`[data-bolt=${obj.btn.getAttribute('data-bolt')}]`).length == 1) {
+      obj.popup.style.top = '';
+      obj.popup.style.left = '';
+    }
+
+    let interactiveEl = document.querySelectorAll(obj.interactiveCSS);
+    // перебираем все элементы
+    for (let i = 0; i < interactiveEl.length; i++) {
+      // если уже есть nabindex
+      if (interactiveEl[i].getAttribute('tabindex')) {
+        // задаем атрибут что бы запомнить nabindex
+        interactiveEl[i].setAttribute('data-tabindex', interactiveEl[i].getAttribute('tabindex'))
+      }
+      // задаем nabindex -1
+      interactiveEl[i].setAttribute('tabindex', -1);
+    }
+
+    let interactivePopup = obj.popup.querySelectorAll('[data-p]');
+
+    for (let i = 0; i < interactivePopup.length; i++) {
+      if (interactivePopup[i].getAttribute('data-tabindex')) {
+        interactivePopup[i].setAttribute('tabindex', interactivePopup[i].getAttribute('data-tabindex'))
+      } else {
+        interactivePopup[i].removeAttribute('tabindex');
+      }
+    }
+
+    interactivePopup[0].focus();
+
+    document.body.style.paddingRight = window.innerWidth - document.body.offsetWidth + 'px';
+
+    document.body.style.top = `-${window.scrollY}px`;
+    document.body.style.position = 'fixed';
+  }
+
+  close(obj, event) {
+    obj.check = false;
+    obj.popup.setAttribute('aria-hidden', true);
+    obj.popup.classList.add('bolt-hidden');
+
+    // объявить в конструкторе, изменять при открытии
+    const scrollY = document.body.style.top;
+    // 
+    console.log(scrollY)
+    document.body.style.position = '';
+    document.body.style.top = '';
+    window.scrollTo(0, parseInt(scrollY || '0') * -1);
+    document.body.style.paddingRight = '';
+
+    let interactivePopup = obj.popup.querySelectorAll('[data-p]');
 
     for (let i = 0; i < interactivePopup.length; i++) {
       if (interactivePopup[i].getAttribute('data-p')) {
         interactivePopup[i].setAttribute('tabindex', -1)
       }
     }
-
     let interactiveEl = document.querySelectorAll('[tabindex="-1"]');
     for (let i = 0; i < interactiveEl.length; i++) {
       if (!interactiveEl[i].getAttribute('data-p')) {
@@ -145,16 +143,20 @@ class Bolt {
         }
       }
     }
-    this.obj.btn.focus();
-
-    if (document.querySelectorAll(`[data-bolt=${this.obj.btn.getAttribute('data-bolt')}]`).length == 1) {
-      var box = this.obj.btn.getBoundingClientRect();
-      this.obj.popup.style.top = box.top + pageYOffset + 'px';
-      this.obj.popup.style.left = box.left + pageXOffset + 'px';
+    obj.btn.focus();
+    if (document.querySelectorAll(`[data-bolt=${obj.btn.getAttribute('data-bolt')}]`).length == 1) {
+      var box = obj.btn.getBoundingClientRect();
+      obj.popup.style.top = box.top + pageYOffset + box.width / 2 + 'px';
+      obj.popup.style.left = box.left + pageXOffset + box.height / 2 + 'px';
     }
+  }
 
-    document.body.style.paddingRight = '';
-    document.body.style.overflow = '';
+  keydownEvent(obj, event) {
+    if (obj.check) {
+      if (event.keyCode == 27) {
+        obj.close(obj);
+      }
+    }
   }
 
 }
